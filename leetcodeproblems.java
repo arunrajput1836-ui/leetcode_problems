d589bdec34b4f1163c87b0b3b6d20569b17ebb00
Arun <arunrajput1836@gmail.com>
2026-09-08 11:43:05 +0530

    Update leetcodeproblems.java

--- leetcodeproblems.java
+++ leetcodeproblems.java
@@ -1,7 +1,24 @@
 # leetcode_problems
 
-#PROBLEM NO 242 (Valid Analog)
+//PROBLEM NO 242 (Valid Analog)
   class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+        return false;
+        char arr1[] = s.toCharArray();
+        char arr2[] = t.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1,arr2);
+        
+    }
+}
+
+
+// 387. First Unique Character in a String
+class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length())
         return false;
